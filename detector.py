368aeb1d09008355233997e9e472fec0ffe5cc68
Osman <osmanaktrk@gmail.com>
2025-09-01 03:03:02 +0200

    Om het debuggen eenvoudiger te maken, is de debug optie toegevoegd en zijn de nodige wijzigingen doorgevoerd voor het debuggen.

--- detector.py
+++ detector.py
@@ -20,6 +20,7 @@ class Detector:
                 finishline_min_radius: int = None, 
                 finishline_max_radius: int = None, 
                 debug: bool = False,
+                is_strict: bool = True,
                 kernel_size: int = 9 
                 ):
             
@@ -34,6 +35,7 @@ class Detector:
         self._finishline_min_radius = finishline_min_radius
         self._finishline_max_radius = finishline_max_radius
         self._debug = debug
+        self._is_strict = is_strict
         self._kernel_size = kernel_size
 
 
@@ -121,13 +123,18 @@ class Detector:
         return self._debug
 
     @debug.setter
-    def debug(self, debug: int):
+    def debug(self, debug: bool):
         self._debug = debug
    
 
+    @property
+    def is_strict(self):
+        return self._is_strict
 
-   
-    
+    @is_strict.setter
+    def is_strict(self, is_strict: bool):
+        self._is_strict = is_strict
+
 
 
 
@@ -137,46 +144,97 @@ class Detector:
         
 
         median_blur = cv.medianBlur(self._finishline_frame.copy(), self._kernel_size)
-        cv.imshow("median_blur", median_blur)
+
+        if self._debug:
+            cv.imshow("median_blur", median_blur)
+        
         #self._gaussian_blur = cv.GaussianBlur(self._frame.copy(), (self._kernel_size, self._kernel_size), 1.4)
         self._hsv = cv.cvtColor(self._finishline_frame.copy(), cv.COLOR_BGR2HSV)
         self._hsv[:, :, 2] = cv.equalizeHist(self._hsv[:, :, 2])
         
+        if self._is_strict:
+            COLORS = COLOR_RANGES_STRICT
+        else:
+            COLORS = COLOR_RANGES
+
+
         match(self._sphero_bolt._color):
 
             case "Red":
-                red1_mask = cv.inRange(self._hsv, COLOR_RANGES["Red1"]["Lower"], COLOR_RANGES["Red1"]["Upper"])
-                red2_mask = cv.inRange(self._hsv, COLOR_RANGES["Red2"]["Lower"], COLOR_RANGES["Red2"]["Upper"])
+                red1_mask = cv.inRange(self._hsv, COLORS["Red1"]["Lower"], COLORS["Red1"]["Upper"])
+                red2_mask = cv.inRange(self._hsv, COLORS["Red2"]["Lower"], COLORS["Red2"]["Upper"])
                 self._mask = cv.bitwise_or(red1_mask, red2_mask)
 
+                masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                if self._debug:
+                    cv.imshow("Red masked_frame", masked_frame)
+                    cv.imshow("Red masked_gray_frame", masked_gray_frame)
+                    cv.imshow("Red masked_frame_threshold", masked_frame_threshold)
+        
+
             case "Yellow":
-                self._mask = cv.inRange(self._hsv, COLOR_RANGES["Yellow"]["Lower"], COLOR_RANGES["Yellow"]["Upper"])
+                self._mask = cv.inRange(self._hsv, COLORS["Yellow"]["Lower"], COLORS["Yellow"]["Upper"])
+
+                masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+
+                if self._debug:
+                    cv.imshow("Yellow masked_frame", masked_frame)
+                    cv.imshow("Yellow masked_gray_frame", masked_gray_frame)
+                    cv.imshow("Yellow masked_frame_threshold", masked_frame_threshold)
+        
+        
+
                 
             case "Green":
-                self._mask = cv.inRange(self._hsv, COLOR_RANGES["Green"]["Lower"], COLOR_RANGES["Green"]["Upper"])
+                self._mask = cv.inRange(self._hsv, COLORS["Green"]["Lower"], COLORS["Green"]["Upper"])
+                
+                masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                
+                if self._debug:
+                    cv.imshow("Green masked_frame", masked_frame)
+                    cv.imshow("Green masked_gray_frame", masked_gray_frame)
+                    cv.imshow("Green masked_frame_threshold", masked_frame_threshold)
+        
+
                 
             case "Blue":
-                self._mask = cv.inRange(self._hsv, COLOR_RANGES["Blue"]["Lower"], COLOR_RANGES["Blue"]["Upper"])
+                self._mask = cv.inRange(self._hsv, COLORS["Blue"]["Lower"], COLORS["Blue"]["Upper"])
+                
+                masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+        
+                if self._debug:
+                    cv.imshow("Blue masked_frame", masked_frame)
+                    cv.imshow("Blue masked_gray_frame", masked_gray_frame)
+                    cv.imshow("Blue masked_frame_threshold", masked_frame_threshold)
+        
+
                 
 
             case "Purple":
-                self._mask = cv.inRange(self._hsv, COLOR_RANGES["Purple"]["Lower"], COLOR_RANGES["Purple"]["Upper"])
+                self._mask = cv.inRange(self._hsv, COLORS["Purple"]["Lower"], COLORS["Purple"]["Upper"])
 
+                masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
 
-
+                if self._debug:
+                    cv.imshow("Purple masked_frame", masked_frame)
+                    cv.imshow("Purple masked_gray_frame", masked_gray_frame)
+                    cv.imshow("Purple masked_frame_threshold", masked_frame_threshold)
         
-        masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
-        cv.imshow("masked frame", masked_frame)
-
-        masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
-        cv.imshow("masked_gray_frame", masked_gray_frame)
-
-        _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
-        cv.imshow("masked_frame_threshold", masked_frame_threshold)
         
        
-
-
         contours, hierarchy = cv.findContours(masked_frame_threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
         
@@ -190,17 +248,11 @@ class Detector:
             y = int(y)
             radius = int(radius)
 
-            print("path", x, y, radius)
-
-            
-
             #self._x, self._y, self._w, self._h = cv.boundingRect(contour)
             #self._radius = int((self._w + self._h)//4)
             #self._x = int((self._x + self._w//2))
             #self._y = int((self._y + self._h//2))
                 
-
-            
 
             if radius >= self._path_min_radius and radius <= self._path_max_radius:
                 
@@ -220,10 +272,12 @@ class Detector:
                         (x, y), COLORS_BGR[self._sphero_bolt.color], 3, cv.LINE_AA)
                 
                 self._sphero_bolt.path_previous_centre = (x, y)
+
                 cv.putText(original_path_frame, self._sphero_bolt.username, (x, y-2*radius), 
                         cv.FONT_HERSHEY_COMPLEX_SMALL, 1, COLORS_BGR[self._sphero_bolt.color], 2, cv.LINE_AA)
                 
-                print(f"area: {area}, x: {x}, y: {y}, radius: {radius}")
+                if self._debug:
+                    print(f"Path, area: {area}, x: {x}, y: {y}, radius: {radius}")
                         
 
 
@@ -240,45 +294,92 @@ class Detector:
             original_finishline_frame = self.finishline_frame.copy()
 
             median_blur = cv.medianBlur(self._finishline_frame.copy(), self._kernel_size)
-            cv.imshow("median_blur", median_blur)
+
+            if self._debug:
+                cv.imshow("median_blur", median_blur)
+
             #self._gaussian_blur = cv.GaussianBlur(self._frame.copy(), (self._kernel_size, self._kernel_size), 1.4)
             self._hsv = cv.cvtColor(self._finishline_frame.copy(), cv.COLOR_BGR2HSV)
             self._hsv[:, :, 2] = cv.equalizeHist(self._hsv[:, :, 2])
+
+                    
+            if self._is_strict:
+                COLORS = COLOR_RANGES_STRICT
+            else:
+                COLORS = COLOR_RANGES
+
             
             match(self._sphero_bolt._color):
 
                 case "Red":
-                    red1_mask = cv.inRange(self._hsv, COLOR_RANGES["Red1"]["Lower"], COLOR_RANGES["Red1"]["Upper"])
-                    red2_mask = cv.inRange(self._hsv, COLOR_RANGES["Red2"]["Lower"], COLOR_RANGES["Red2"]["Upper"])
+                    red1_mask = cv.inRange(self._hsv, COLORS["Red1"]["Lower"], COLORS["Red1"]["Upper"])
+                    red2_mask = cv.inRange(self._hsv, COLORS["Red2"]["Lower"], COLORS["Red2"]["Upper"])
                     self._mask = cv.bitwise_or(red1_mask, red2_mask)
+                    
+                    masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                    masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                    _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                    if self._debug:
+                        cv.imshow("Red masked frame", masked_frame)
+                        cv.imshow("Red masked_gray_frame", masked_gray_frame)
+                        cv.imshow("Red masked_frame_threshold", masked_frame_threshold)
+                    
+
 
                 case "Yellow":
-                    self._mask = cv.inRange(self._hsv, COLOR_RANGES["Yellow"]["Lower"], COLOR_RANGES["Yellow"]["Upper"])
+                    self._mask = cv.inRange(self._hsv, COLORS["Yellow"]["Lower"], COLORS["Yellow"]["Upper"])
+           
+                    masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                    masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                    _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                    if self._debug:
+                        cv.imshow("Yellow masked frame", masked_frame)
+                        cv.imshow("Yellow masked_gray_frame", masked_gray_frame)
+                        cv.imshow("Yellow masked_frame_threshold", masked_frame_threshold)
+            
                     
                 case "Green":
-                    self._mask = cv.inRange(self._hsv, COLOR_RANGES["Green"]["Lower"], COLOR_RANGES["Green"]["Upper"])
+                    self._mask = cv.inRange(self._hsv, COLORS["Green"]["Lower"], COLORS["Green"]["Upper"])
+       
+                    masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                    masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                    _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                    if self._debug:
+                        cv.imshow("Green masked frame", masked_frame)
+                        cv.imshow("Green masked_gray_frame", masked_gray_frame)
+                        cv.imshow("Green masked_frame_threshold", masked_frame_threshold)
+            
                     
                 case "Blue":
-                    self._mask = cv.inRange(self._hsv, COLOR_RANGES["Blue"]["Lower"], COLOR_RANGES["Blue"]["Upper"])
+                    self._mask = cv.inRange(self._hsv, COLORS["Blue"]["Lower"], COLORS["Blue"]["Upper"])
+           
+                    masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                    masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                    _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
+
+                    if self._debug:
+                        cv.imshow("Blue masked frame", masked_frame)
+                        cv.imshow("Blue masked_gray_frame", masked_gray_frame)
+                        cv.imshow("Blue masked_frame_threshold", masked_frame_threshold)
+            
                     
 
                 case "Purple":
-                    self._mask = cv.inRange(self._hsv, COLOR_RANGES["Purple"]["Lower"], COLOR_RANGES["Purple"]["Upper"])
+                    self._mask = cv.inRange(self._hsv, COLORS["Purple"]["Lower"], COLORS["Purple"]["Upper"])
 
+                    masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
+                    masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
+                    _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
 
-
+                    if self._debug:
+                        cv.imshow("Purple masked frame", masked_frame)
+                        cv.imshow("Purple masked_gray_frame", masked_gray_frame)
+                        cv.imshow("Purple masked_frame_threshold", masked_frame_threshold)
             
-            masked_frame = cv.bitwise_and(median_blur.copy(), median_blur.copy(), mask=self._mask)
-            cv.imshow("masked frame", masked_frame)
-
-            masked_gray_frame = cv.cvtColor(masked_frame.copy(), cv.COLOR_BGR2GRAY)
-            cv.imshow("masked_gray_frame", masked_gray_frame)
-
-            _, masked_frame_threshold= cv.threshold(masked_gray_frame.copy(), 50, 255, cv.THRESH_BINARY)
-            cv.imshow("masked_frame_threshold", masked_frame_threshold)
             
-        
-
 
             contours, hierarchy = cv.findContours(masked_frame_threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
@@ -292,10 +393,6 @@ class Detector:
                 x = int(x)
                 y = int(y)
                 radius = int(radius)
-
-                print("finishline", x, y, radius)
-
-                
 
                 #self._x, self._y, self._w, self._h = cv.boundingRect(contour)
                 #self._radius = int((self._w + self._h)//4)
@@ -314,7 +411,8 @@ class Detector:
                     cv.putText(original_finishline_frame, self._sphero_bolt.username, (x, y-2*radius), 
                             cv.FONT_HERSHEY_COMPLEX_SMALL, 1, COLORS_BGR[self._sphero_bolt.color], 2, cv.LINE_AA)
                         
-
+                if self._debug:
+                    print(f"Finishline, area: {area}, x: {x}, y: {y}, radius: {radius}")
 
 
 

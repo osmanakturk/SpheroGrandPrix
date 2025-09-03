9d1e41816518c0a726d3045bb60c386390cb3324
Osman <osmanaktrk@gmail.com>
2025-09-03 18:18:37 +0200

    Bijgewerkt om rekening te houden met veranderende projectvereisten: start- en stoplijnen zijn gescheiden.

--- tools/finishline_camera_perspective.py
+++ tools/finishline_camera_perspective.py
@@ -13,7 +13,7 @@ def trackbar_callback(val):
 
 
 
-#cap = cv.VideoCapture("./sphero/sphero1.mp4")
+
 cap = cv.VideoCapture(1, cv.CAP_DSHOW)
 
 cap.set(cv.CAP_PROP_SETTINGS, 1)
@@ -48,7 +48,13 @@ cv.createTrackbar("BR_x", finishline_trackbar, 0, FRAME_WIDTH, trackbar_callback
 cv.createTrackbar("BR_y", finishline_trackbar, 0, FRAME_HEIGHT, trackbar_callback)
 
 
-cv.createTrackbar("F_y", finishline_perspective, 0, FRAME_HEIGHT, trackbar_callback)
+cv.createTrackbar("start_y", finishline_trackbar, 0, FRAME_HEIGHT, trackbar_callback)
+cv.createTrackbar("start_w", finishline_trackbar, 0, FRAME_WIDTH, trackbar_callback)
+
+
+cv.createTrackbar("stop_y", finishline_trackbar, 0, FRAME_HEIGHT, trackbar_callback)
+cv.createTrackbar("stop_w", finishline_trackbar, 0, FRAME_WIDTH, trackbar_callback)
+
 
 while cap.isOpened():
     
@@ -74,8 +80,11 @@ while cap.isOpened():
     br_y = int(cv.getTrackbarPos("BR_y", finishline_trackbar))
 
 
-    finishline = int(cv.getTrackbarPos("F_y", finishline_perspective))
-    
+    start_line_y = int(cv.getTrackbarPos("start_y", finishline_trackbar))
+    start_line_w = int(cv.getTrackbarPos("start_w", finishline_trackbar))
+
+    stop_line_y = int(cv.getTrackbarPos("stop_y", finishline_trackbar))
+    stop_line_w = int(cv.getTrackbarPos("stop_w", finishline_trackbar))
 
     
     canvas = np.full_like(frame, 0, np.uint8)
@@ -96,13 +105,13 @@ while cap.isOpened():
     cv.putText(frame, f"BR:{br_x, br_y}", (br_x, br_y+15), cv.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1, cv.LINE_AA)
     
 
-    if any([tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y, finishline]):
+    if any([tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y, start_line_y, stop_line_y]):
 
         x_max = max(abs(tl_x - tr_x), abs(bl_x - br_x)) | 0
         y_max = max(abs(tl_y - bl_y), abs(tr_y - br_y)) | 0
 
-        width = x_max
-        height = y_max
+        width = int(x_max)
+        height = int(y_max)
 
 
 
@@ -116,10 +125,15 @@ while cap.isOpened():
                             [width, height], 
                             [0, height]], dtype=np.float32)
 
+    
         matrix = cv.getPerspectiveTransform(pts_src, pts_dst)
         perspective = cv.warpPerspective(frame, matrix, (width, height))
-        cv.line(perspective, (0, finishline), (width, finishline), (0, 0, 255), 2, cv.LINE_AA)
-        cv.putText(perspective, f"Finishline y:({finishline})", (0, finishline-15), cv.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1, cv.LINE_AA)
+        cv.line(perspective, (0, start_line_y), (start_line_w, start_line_y), (0, 0, 255), 2, cv.LINE_AA)
+        cv.line(perspective, (stop_line_w, stop_line_y), (width, stop_line_y), (0, 0, 255), 2, cv.LINE_AA)
+
+        cv.putText(perspective, f"Start:({start_line_y})", (0, start_line_y-15), cv.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1, cv.LINE_AA)
+        cv.putText(perspective, f"Stop:({stop_line_y})", (stop_line_w, stop_line_y-15), cv.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1, cv.LINE_AA)
+
         cv.imshow(finishline_perspective, perspective)
 
     
@@ -142,7 +156,8 @@ while cap.isOpened():
             print(f"Top_Right: {tr_x, tr_y}") 
             print(f"Bottom-Left: {bl_x, bl_y}") 
             print(f"Bottom-Right: {br_x, br_y}")
-            print(f"Finishline y: ({finishline})")
+            print(f"Start_Line: ({0, start_line_y}), ({start_line_w, start_line_y})")
+            print(f"Stop_Line: ({stop_line_w, stop_line_y}), ({width, stop_line_y})")
             print(f"Area: [{min(tl_y, tr_y)}:{max(bl_y, br_y)}, {min(tl_x, bl_x)}:{max(tr_x, br_x)}]")
             print(f"x_max: {x_max}, y_max: {y_max}")
             print(f"width: {width} height: {height}")

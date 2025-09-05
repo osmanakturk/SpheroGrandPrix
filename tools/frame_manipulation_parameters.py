d1c96f8af7abdd111be11a6dd74475ba46d9bf5f
Osman <osmanaktrk@gmail.com>
2025-09-05 06:47:22 +0200

    Clahe filter en morfologie filter zijn toegevoegd naar aanleiding van veranderende behoeften. Typefouten zijn gecorrigeerd.

--- tools/frame_manipulation_parameters.py
+++ tools/frame_manipulation_parameters.py
@@ -4,7 +4,7 @@ from backend.constants import COLOR_RANGES_STRICT, COLOR_RANGES_WIDE, COLOR_RANG
 
 
 
-COLORS = COLOR_RANGES_WIDE
+COLORS = COLOR_RANGES_NORMAL
 
 def trackbar(_):
     pass
@@ -15,14 +15,19 @@ def main():
 
     cap = cv.VideoCapture(1, cv.CAP_DSHOW)
 
-    cv.namedWindow("Trackbar", cv.WINDOW_NORMAL)
+    cv.namedWindow("Trackbar", cv.WINDOW_AUTOSIZE)
     cv.namedWindow("Camera", cv.WINDOW_KEEPRATIO)
 
-    cv.createTrackbar("briteral_d", "Trackbar", 1, 255, trackbar)
-    cv.createTrackbar("median_k", "Trackbar", 1, 255, trackbar)
+    cv.createTrackbar("bilateral_d", "Trackbar", 1, 50, trackbar)
+    cv.createTrackbar("median_k", "Trackbar", 1, 50, trackbar)
     cv.createTrackbar("s_color", "Trackbar", 1, 255, trackbar)
     cv.createTrackbar("s_space", "Trackbar", 1, 255, trackbar)
 
+    cv.createTrackbar("clahe_clip", "Trackbar", 1, 50, trackbar)
+    cv.createTrackbar("clahe_grid", "Trackbar", 2, 50, trackbar)
+
+    cv.createTrackbar("morph_ksize", "Trackbar", 5, 50, trackbar)
+    cv.createTrackbar("morph_iter", "Trackbar", 1, 10, trackbar)
 
 
     try:
@@ -34,18 +39,24 @@ def main():
 
             
         
-            briteral_d = int(cv.getTrackbarPos("briteral_d", "Trackbar"))
-            briteral_d = briteral_d if briteral_d % 2 == 1 else briteral_d +1
+            bilateral_d = int(cv.getTrackbarPos("bilateral_d", "Trackbar"))
+            bilateral_d = bilateral_d if bilateral_d % 2 == 1 else bilateral_d +1
             median_k = int(cv.getTrackbarPos("median_k", "Trackbar"))
             median_k = median_k if median_k % 2 == 1 else median_k + 1
             sigma_color = int(cv.getTrackbarPos("s_color", "Trackbar"))
             sigma_color = sigma_color if sigma_color >= 1 else 1
             sigma_space = int(cv.getTrackbarPos("s_space", "Trackbar"))
             sigma_space = sigma_space if sigma_space >= 1 else 1
+            clahe_clip = max(1, cv.getTrackbarPos("clahe_clip", "Trackbar"))
+            clahe_grid = max(2, cv.getTrackbarPos("clahe_grid", "Trackbar"))
+            morph_kernel_size = max(1, cv.getTrackbarPos("morph_ksize", "Trackbar"))
+            morph_iterator = max(1, cv.getTrackbarPos("morph_iter", "Trackbar"))
 
 
 
-            bilateral_blur = cv.bilateralFilter(frame, briteral_d, sigma_color, sigma_space)
+
+
+            bilateral_blur = cv.bilateralFilter(frame, bilateral_d, sigma_color, sigma_space)
             
 
 
@@ -55,16 +66,35 @@ def main():
 
 
             hsv = cv.cvtColor(bilateral_blur, cv.COLOR_BGR2HSV)
-            hsv[:, :, 2] = cv.equalizeHist(hsv[:, :, 2])
 
+            h, s, v = cv.split(hsv)
+
+            clahe = cv.createCLAHE(clipLimit=float(clahe_clip), tileGridSize=(clahe_grid, clahe_grid))
+            v_clahe = clahe.apply(v)
+
+            hsv = cv.merge([h, s, v_clahe])
+
+            frame_clahe = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+
+            cv.imshow("Frame Clahe", frame_clahe)
+
+            
 
             red1_mask = cv.inRange(hsv, COLORS["Red1"]["Lower"], COLORS["Red1"]["Upper"])
             red2_mask = cv.inRange(hsv, COLORS["Red2"]["Lower"], COLORS["Red2"]["Upper"])
-
             red_mask = cv.bitwise_or(red1_mask, red2_mask)
+            
+
             yellow_mask = cv.inRange(hsv, COLORS["Yellow"]["Lower"], COLORS["Yellow"]["Upper"])
+
+
             blue_mask = cv.inRange(hsv, COLORS["Blue"]["Lower"], COLORS["Blue"]["Upper"])
+
+
+
             green_mask = cv.inRange(hsv, COLORS["Green"]["Lower"], COLORS["Green"]["Upper"])
+
+
 
 
             cv.imshow("Red Mask", red_mask)
@@ -72,11 +102,27 @@ def main():
             cv.imshow("Blue Mask", blue_mask)
             cv.imshow("Green Mask", green_mask)
 
+            morph_ellipse_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (morph_kernel_size, morph_kernel_size))
+
 
             red_mask_median = cv.medianBlur(red_mask, median_k)
+
+            red_mask_median_morph = cv.morphologyEx(red_mask_median, cv.MORPH_OPEN, morph_ellipse_kernel, iterations=morph_iterator)
+            red_mask_median_morph = cv.morphologyEx(red_mask_median_morph, cv.MORPH_CLOSE, morph_ellipse_kernel, iterations=morph_iterator)
+
             yellow_mask_median = cv.medianBlur(yellow_mask, median_k)
+            yellow_mask_median_morph = cv.morphologyEx(yellow_mask_median, cv.MORPH_OPEN, morph_ellipse_kernel, iterations=morph_iterator)
+            yellow_mask_median_morph = cv.morphologyEx(yellow_mask_median_morph, cv.MORPH_CLOSE, morph_ellipse_kernel, iterations=morph_iterator)
+
             blue_mask_median = cv.medianBlur(blue_mask, median_k)
+            blue_mask_median_morph = cv.morphologyEx(blue_mask_median, cv.MORPH_OPEN, morph_ellipse_kernel, iterations=morph_iterator)
+            blue_mask_median_morph = cv.morphologyEx(blue_mask_median_morph, cv.MORPH_CLOSE, morph_ellipse_kernel, iterations=morph_iterator)
+
+
             green_mask_median = cv.medianBlur(green_mask, median_k)
+            green_mask_median_morph = cv.morphologyEx(green_mask_median, cv.MORPH_OPEN, morph_ellipse_kernel, iterations=morph_iterator)
+            green_mask_median_morph = cv.morphologyEx(green_mask_median_morph, cv.MORPH_CLOSE, morph_ellipse_kernel, iterations=morph_iterator)
+
 
             cv.imshow("Red Mask Median", red_mask_median)
             cv.imshow("Yellow Mask Median", yellow_mask_median)
@@ -84,11 +130,17 @@ def main():
             cv.imshow("Green Mask Median", green_mask_median)
 
 
+            cv.imshow("Red Mask Median Morphology", red_mask_median_morph)
+            cv.imshow("Yellow Mask Median Morphology", yellow_mask_median_morph)
+            cv.imshow("Blue Mask Median Morphology", blue_mask_median_morph)
+            cv.imshow("Green Mask Median Morphology", green_mask_median_morph)
 
-            red = cv.bitwise_and(frame, frame, mask=red_mask_median)
-            yellow = cv.bitwise_and(frame, frame, mask=yellow_mask_median)
-            blue = cv.bitwise_and(frame, frame, mask=blue_mask_median)
-            green = cv.bitwise_and(frame, frame, mask=green_mask_median)
+
+
+            red = cv.bitwise_and(frame, frame, mask=red_mask_median_morph)
+            yellow = cv.bitwise_and(frame, frame, mask=yellow_mask_median_morph)
+            blue = cv.bitwise_and(frame, frame, mask=blue_mask_median_morph)
+            green = cv.bitwise_and(frame, frame, mask=green_mask_median_morph)
 
             cv.imshow("Red", red)
             cv.imshow("Yellow", yellow)
@@ -102,16 +154,16 @@ def main():
             blue_contours_frame = frame.copy()
 
 
-            green_contours, green_hierarchy = cv.findContours(green_mask_median, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+            green_contours, _ = cv.findContours(green_mask_median_morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
             cv.drawContours(green_contours_frame, green_contours, -1, (0, 0, 255), 3)
 
-            yellow_contours, yellow_hierarchy = cv.findContours(yellow_mask_median, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+            yellow_contours, _ = cv.findContours(yellow_mask_median_morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
             cv.drawContours(yellow_contours_frame, yellow_contours, -1, (0, 0, 255), 3)
             
-            red_contours, red_hierarchy = cv.findContours(red_mask_median, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+            red_contours, _ = cv.findContours(red_mask_median_morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
             cv.drawContours(red_contours_frame, red_contours, -1, (0, 0, 255), 3)
             
-            blue_contours, blue_hierarchy = cv.findContours(blue_mask_median, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+            blue_contours, _ = cv.findContours(blue_mask_median_morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
             cv.drawContours(blue_contours_frame, blue_contours, -1, (0, 0, 255), 3)
     
             
@@ -128,10 +180,14 @@ def main():
                 break
             elif key == ord("c"):
                 print("*"*20)
-                print(f"Briteral Diameter: {briteral_d}")
-                print(f"Briteral Sigma Color: {sigma_color}")
-                print(f"Briteral Sigma Space: {sigma_space}")
+                print(f"Bilateral Diameter: {bilateral_d}")
+                print(f"Bilateral Sigma Color: {sigma_color}")
+                print(f"Bilateral Sigma Space: {sigma_space}")
                 print(f"Median Kernel Size: {median_k}")
+                print(f"Clahe Clip Limit Size: {clahe_clip}")
+                print(f"Clahe Tile Grid Size: {clahe_grid}")
+                print(f"Morphology Kernel Size: {morph_kernel_size}")
+                print(f"Morphology Iterator: {morph_iterator}")
                 print("*"*20)
     finally:
         cap.release()

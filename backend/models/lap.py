2e8e7601d6ae64cf7bfdf84a4c992fec091b85eb
Osman <osmanaktrk@gmail.com>
2025-09-10 00:34:48 +0200

    Om het beheer ervan te vereenvoudigen, zijn start- en stop functies aan het Lap-object toegevoegd. Ook zijn de nodige wijzigingen doorgevoerd.

--- backend/models/lap.py
+++ backend/models/lap.py
@@ -1,5 +1,4 @@
-import uuid
-import time
+import uuid, sqlite3, time
 import numpy as np
 import cv2 as cv
 from datetime import datetime
@@ -11,111 +10,23 @@ from typing import Optional, Tuple
 
 class Lap:
 
-    def __init__(self, 
-                 debug_red: bool = False, 
-                 debug_yellow: bool = False, 
-                 debug_blue: bool = False, 
-                 debug_green: bool = False):
+    def __init__(self):
         
-        self._debug_red = debug_red
-        self._debug_yellow = debug_yellow
-        self._debug_blue = debug_blue
-        self._debug_green = debug_green
+
         self._id: Optional[str] = None
         self._is_started: bool = False
         self._is_stopped: bool = False
-        self._path_frame: Optional[cv.typing.MatLike]  = None 
-        self._finishline_frame: Optional[cv.typing.MatLike] = None 
-        self._background_img: Optional[cv.typing.MatLike] = None 
-        self._sphero_bolt_red: Optional[SpheroBolt] = None 
-        self._sphero_bolt_green: Optional[SpheroBolt] = None 
-        self._sphero_bolt_yellow: Optional[SpheroBolt] = None 
+        self._debug_red: bool = False
+        self._debug_yellow: bool = False
+        self._debug_blue: bool = False
+        self._debug_green: bool = False
+        self._background_img: Optional[cv.typing.MatLike]  = None
+        self._path_frame: Optional[cv.typing.MatLike]  = None
+        self._finishline_frame: Optional[cv.typing.MatLike] = None
+        self._sphero_bolt_red: Optional[SpheroBolt] = None
+        self._sphero_bolt_green: Optional[SpheroBolt] = None
+        self._sphero_bolt_yellow: Optional[SpheroBolt] = None
         self._sphero_bolt_blue: Optional[SpheroBolt] = None
-
-
-
-    def start(self,
-                path_frame: Optional[cv.typing.MatLike]  = None,
-                finishline_frame: Optional[cv.typing.MatLike] = None, 
-                background_img: Optional[cv.typing.MatLike] = None, 
-                debug_red: bool = False, 
-                debug_yellow: bool = False, 
-                debug_blue: bool = False, 
-                debug_green: bool = False
-                ) -> bool:
-
-        if self._is_started:
-            return False
-            
-
-        self._path_frame = path_frame.copy() if path_frame is not None else None
-        self._finishline_frame = finishline_frame.copy() if finishline_frame is not None else None
-        self._background_img = background_img.copy() if background_img is not None else None        
-        self._id = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}_{uuid.uuid4().hex}"
-        self._is_started = True
-        self._is_stopped = False
-        self._debug_red = debug_red
-        self._debug_yellow = debug_yellow
-        self._debug_blue = debug_blue
-        self._debug_green = debug_green
-
-
-        self._sphero_bolt_red = SpheroBolt(
-            lap_id=self._id, 
-            color=SpheroColor.RED, 
-            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
-            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
-            background=self._background_img.copy() if self._background_img is not None else None, 
-            is_lap_started=self._is_started, 
-            is_lap_stopped=self._is_stopped, 
-            debug=self._debug_red
-            )
-
-
-
-        self._sphero_bolt_yellow = SpheroBolt(
-            lap_id=self._id, 
-            color=SpheroColor.YELLOW, 
-            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
-            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
-            background=self._background_img.copy() if self._background_img is not None else None, 
-            is_lap_started=self._is_started, 
-            is_lap_stopped=self._is_stopped, 
-            debug=self._debug_yellow
-            )
-
-
-
-        self._sphero_bolt_blue = SpheroBolt(
-            lap_id=self._id, 
-            color=SpheroColor.BLUE, 
-            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
-            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
-            background=self._background_img.copy() if self._background_img is not None else None, 
-            is_lap_started=self._is_started, 
-            is_lap_stopped=self._is_stopped, 
-            debug=self._debug_blue
-            )
-
-
-
-
-        self._sphero_bolt_green = SpheroBolt(
-            lap_id=self._id, 
-            color=SpheroColor.GREEN, 
-            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
-            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None,  
-            background=self._background_img.copy() if self._background_img is not None else None, 
-            is_lap_started=self._is_started, 
-            is_lap_stopped=self._is_stopped, 
-            debug=self._debug_green
-            )
-        
-        return True
-
-        
-    
-
 
 
 
@@ -149,9 +60,9 @@ class Lap:
 
 
     @property
-    def id(self) -> str:
+    def id(self) -> Optional[str]:
         if self._id is None:
-            raise RuntimeError("Lap is not started")
+            print("Lap is not started")
         return self._id
     
     @property 
@@ -248,11 +159,121 @@ class Lap:
         return self._sphero_bolt_blue
     
 
+
+  
+    def _start_impl(self,
+              path_frame: Optional[cv.typing.MatLike]  = None,
+              finishline_frame: Optional[cv.typing.MatLike] = None, 
+              background_img: Optional[cv.typing.MatLike] = None, 
+              debug_red: Optional[bool] = False, 
+              debug_yellow: Optional[bool] = False, 
+              debug_blue: Optional[bool] = False, 
+              debug_green: Optional[bool] = False) -> bool:
+
+        if self._is_started and not self._is_stopped:
+            return False
+            
+
+        
+        self._id = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}_{uuid.uuid4().hex}"
+        self._is_started = True
+        self._is_stopped = False
+
+        self._path_frame = path_frame.copy() if path_frame is not None else None
+        self._finishline_frame = finishline_frame.copy() if finishline_frame is not None else None
+        self._background_img = background_img.copy() if background_img is not None else None
+        self._debug_red = debug_red
+        self._debug_yellow = debug_yellow
+        self._debug_blue = debug_blue
+        self._debug_green = debug_green
+
+
+
+        self._sphero_bolt_red = SpheroBolt(
+            lap_id=self._id, 
+            color=SpheroColor.RED, 
+            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
+            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
+            background=self._background_img.copy() if self._background_img is not None else None, 
+            is_lap_started=self._is_started, 
+            is_lap_stopped=self._is_stopped, 
+            debug=self._debug_red
+            )
+
+
+
+        self._sphero_bolt_yellow = SpheroBolt(
+            lap_id=self._id, 
+            color=SpheroColor.YELLOW, 
+            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
+            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
+            background=self._background_img.copy() if self._background_img is not None else None, 
+            is_lap_started=self._is_started, 
+            is_lap_stopped=self._is_stopped, 
+            debug=self._debug_yellow
+            )
+
+
+
+        self._sphero_bolt_blue = SpheroBolt(
+            lap_id=self._id, 
+            color=SpheroColor.BLUE, 
+            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
+            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None, 
+            background=self._background_img.copy() if self._background_img is not None else None, 
+            is_lap_started=self._is_started, 
+            is_lap_stopped=self._is_stopped, 
+            debug=self._debug_blue
+            )
+
+
+
+
+        self._sphero_bolt_green = SpheroBolt(
+            lap_id=self._id, 
+            color=SpheroColor.GREEN, 
+            path_frame=self._path_frame.copy() if self._path_frame is not None else None, 
+            finishline_frame=self._finishline_frame.copy() if self._finishline_frame is not None else None,  
+            background=self._background_img.copy() if self._background_img is not None else None, 
+            is_lap_started=self._is_started, 
+            is_lap_stopped=self._is_stopped, 
+            debug=self._debug_green
+            )
+        
+        return True
+
+        
+    @classmethod
+    def start(cls, 
+              path_frame: Optional[cv.typing.MatLike]  = None,
+              finishline_frame: Optional[cv.typing.MatLike] = None, 
+              background_img: Optional[cv.typing.MatLike] = None, 
+              debug_red: Optional[bool] = False, 
+              debug_yellow: Optional[bool] = False, 
+              debug_blue: Optional[bool] = False, 
+              debug_green: Optional[bool] = False) -> "Lap":
+        
+        lap = cls()
+        
+        lap._start_impl(path_frame=path_frame, 
+                        finishline_frame=finishline_frame, 
+                        background_img= background_img, 
+                        debug_red=debug_red, 
+                        debug_yellow=debug_yellow,
+                        debug_blue=debug_blue, 
+                        debug_green=debug_green)
+        print(f"Lap started with id: {lap._id}")
+        return lap
+
+
+
+
+
     def _sphero_bolts(self) -> Tuple[SpheroBolt, SpheroBolt, SpheroBolt, SpheroBolt]:
         
-        return (self._sphero_bolt_yellow, 
+        return (self._sphero_bolt_red, 
+                self._sphero_bolt_yellow, 
                 self._sphero_bolt_green, 
-                self._sphero_bolt_red, 
                 self._sphero_bolt_blue)
 
 
@@ -273,7 +294,8 @@ class Lap:
             ) -> Optional[cv.typing.MatLike]:
 
         if not self._is_started:
-            raise RuntimeError("Lap not started yet")
+            print("Lap not started yet")
+            return None
 
         yellow = self._sphero_bolt_yellow.get_processed_path_frame(
             hsv_ranges=hsv_ranges, 
@@ -397,7 +419,8 @@ class Lap:
 
         
         if not self._is_started:
-            raise RuntimeError("Lap not started yet")
+            print("Lap not started yet")
+            return None
 
         yellow = self._sphero_bolt_yellow.get_processed_finishline_frame(
             hsv_ranges=hsv_ranges, 
@@ -507,3 +530,77 @@ class Lap:
         return frame
 
 
+    def _save(self) -> bool:
+
+        if not self._is_stopped:
+            print("Lap could not be saved")
+            return False
+
+        try:
+
+            with sqlite3.connect("database.sqlite") as db:
+
+                cursor = db.cursor()
+
+                cursor.execute("""CREATE TABLE IF NOT EXISTS sphero_bolt(
+                               id TEXT PRIMARY KEY NOT NULL, 
+                               lap_id TEXT NOT NULL, 
+                               color TEXT NOT NULL, 
+                               username TEXT NOT NULL, 
+                               path_img_path TEXT, 
+                               start_time TEXT, 
+                               finish_time TEXT, 
+                               total_lap_time REAL
+                               )""")
+
+                cursor.execute("CREATE INDEX IF NOT EXISTS idx_sphero_bolt_lap_id ON sphero_bolt(lap_id)")
+
+
+                sphero_bots = self._sphero_bolts()
+
+                for sphero in sphero_bots:
+
+                    if sphero is not None:
+
+                        sphero_id = sphero.id 
+                        lap_id = sphero.lap_id 
+                        color = sphero.color.value 
+                        username = sphero.username 
+                        path_img_path = f"paths/{sphero.id}.png" if sphero.path_frame is not None and sphero.total_lap_time is not None else None
+                        start_time = sphero.start_time.isoformat() if sphero.start_time is not None else None 
+                        finish_time = sphero.finish_time.isoformat() if sphero.finish_time is not None else None 
+                        total_lap_time = sphero.total_lap_time
+
+                        cursor.execute("""INSERT INTO sphero_bolt 
+                                       (id, lap_id, color, username, path_img_path, start_time, finish_time, total_lap_time) 
+                                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", 
+                                       (sphero_id, lap_id, color, username, path_img_path, start_time, finish_time, total_lap_time)
+                                       )
+
+
+                print("Lap saved")
+            return True
+        except Exception as e:
+            print("Lap could not be saved")
+            print(e)
+            return False
+
+
+    def stop(self) -> bool:
+        
+        if not self._is_started:
+            print("Lap not started yet")
+            return False
+        
+        print(f"Lap stoppen with id: {self._id}")
+        
+        self._is_stopped = True
+        self._save()
+        self._is_started = False
+        self._sphero_bolt_red = None
+        self._sphero_bolt_yellow = None
+        self._sphero_bolt_blue = None
+        self._sphero_bolt_green = None
+        self._background_img = None
+        self._id = None
+        return True

5e8985a3296f45001fe6ade04a95d616bb982381
Osman <osmanaktrk@gmail.com>
2025-10-13 14:50:33 +0200

    bug fixed

--- frontend/static/js/game.js
+++ frontend/static/js/game.js
@@ -78,7 +78,7 @@ async function updateStats() {
         let total_blue = document.getElementById("total_blue");
         let total_green = document.getElementById("total_green");
         let dashboard_list = document.getElementById("dashboard_list")
-
+        dashboard_list.innerText = ""
 
         total_games.textContent = totals.games ?? 0
         total_spheros.textContent = totals.spheros ?? 0
